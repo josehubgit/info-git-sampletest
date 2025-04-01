98685eceb20c03c24a446f9eb77315532129e141
Jose Joseph <josegit0110@gmail.com>
2025-04-01 17:02:54 +0530

    The first imp commit

---
+++ info-git-sampletest/src/main/java/org/testgit/info_git_sampletest/App.java
@@ -0,0 +1,10 @@
+package org.testgit.info_git_sampletest;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ info-git-sampletest/src/main/java/org/testgit/info_git_sampletest/Samplegittest.java
@@ -0,0 +1,10 @@
+package org.testgit.info_git_sampletest;
+
+public class Samplegittest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
